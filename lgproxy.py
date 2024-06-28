12c132603e01e0981d64a1a879907c0510d12a7f
Andreas Düring <github@duering-andreas.de>
2024-06-28 17:49:38 +0200

    2to3 and format lgproxypip install 2to3

--- lgproxy.py
+++ lgproxy.py
@@ -27,6 +27,7 @@ from logging.handlers import TimedRotatingFileHandler
 from logging import FileHandler
 import subprocess
 from urllib.parse import unquote
+
 # import argparse
 
 from bird import BirdSocket
@@ -40,29 +41,50 @@ from flask import Flask, request, abort
 
 app = Flask(__name__)
 app.debug = app.config["DEBUG"]
-app.config.from_pyfile('lgproxy.cfg')
+app.config.from_pyfile("lgproxy.cfg")
 
-file_handler = TimedRotatingFileHandler(filename=app.config["LOG_FILE"], when="midnight", backupCount=app.config.get("LOG_NUM_DAYS", 0))
+file_handler = TimedRotatingFileHandler(
+    filename=app.config["LOG_FILE"],
+    when="midnight",
+    backupCount=app.config.get("LOG_NUM_DAYS", 0),
+)
 app.logger.setLevel(getattr(logging, app.config["LOG_LEVEL"].upper()))
 app.logger.addHandler(file_handler)
 
+
 @app.before_request
 def access_log_before(*args, **kwargs):
-    app.logger.info("[%s] request %s, %s", request.remote_addr, request.url, "|".join(["%s:%s"%(k,v) for k,v in request.headers.items()]))
+    app.logger.info(
+        "[%s] request %s, %s",
+        request.remote_addr,
+        request.url,
+        "|".join(["%s:%s" % (k, v) for k, v in list(request.headers.items())]),
+    )
+
 
 @app.after_request
 def access_log_after(response, *args, **kwargs):
-    app.logger.info("[%s] reponse %s, %s", request.remote_addr,  request.url, response.status_code)
+    app.logger.info(
+        "[%s] reponse %s, %s", request.remote_addr, request.url, response.status_code
+    )
     return response
 
+
 def check_security():
-    if app.config["ACCESS_LIST"] and request.remote_addr not in app.config["ACCESS_LIST"]:
+    if (
+        app.config["ACCESS_LIST"]
+        and request.remote_addr not in app.config["ACCESS_LIST"]
+    ):
         app.logger.info("Your remote address is not valid")
         abort(401)
 
-    if app.config.get('SHARED_SECRET') and request.args.get("secret") != app.config["SHARED_SECRET"]:
+    if (
+        app.config.get("SHARED_SECRET")
+        and request.args.get("secret") != app.config["SHARED_SECRET"]
+    ):
         app.logger.info("Your shared secret is not valid")
         abort(401)
+
 
 @app.route("/traceroute")
 @app.route("/traceroute6")
@@ -70,34 +92,43 @@ def traceroute():
     return "Not allowed."
     check_security()
 
-    if sys.platform.startswith('freebsd') or sys.platform.startswith('netbsd') or sys.platform.startswith('openbsd'):
-        traceroute4 = [ 'traceroute' ]
-        traceroute6 = [ 'traceroute6' ]
-    else: # For Linux
-        traceroute4 = [ 'traceroute', '-4' ]
-        traceroute6 = [ 'traceroute', '-6' ]
+    if (
+        sys.platform.startswith("freebsd")
+        or sys.platform.startswith("netbsd")
+        or sys.platform.startswith("openbsd")
+    ):
+        traceroute4 = ["traceroute"]
+        traceroute6 = ["traceroute6"]
+    else:  # For Linux
+        traceroute4 = ["traceroute", "-4"]
+        traceroute6 = ["traceroute", "-6"]
 
     src = []
-    if request.path == '/traceroute6':
+    if request.path == "/traceroute6":
         traceroute = traceroute6
         if app.config.get("IPV6_SOURCE", ""):
-            src = [ "-s",  app.config.get("IPV6_SOURCE") ]
-    else: 
+            src = ["-s", app.config.get("IPV6_SOURCE")]
+    else:
         traceroute = traceroute4
-        if app.config.get("IPV4_SOURCE",""):
-            src = [ "-s",  app.config.get("IPV4_SOURCE") ]
+        if app.config.get("IPV4_SOURCE", ""):
+            src = ["-s", app.config.get("IPV4_SOURCE")]
 
-    query = request.args.get("q","")
+    query = request.args.get("q", "")
     query = unquote(query)
 
-    if sys.platform.startswith('freebsd') or sys.platform.startswith('netbsd'):
-        options = [ '-a', '-q1', '-w1', '-m15' ]
-    elif sys.platform.startswith('openbsd'):
-        options = [ '-A', '-q1', '-w1', '-m15' ]
-    else: # For Linux
-        options = [ '-A', '-q1', '-N32', '-w1', '-m15' ]
-    command = traceroute + src + options + [ query ]
-    result = subprocess.Popen( command , stdout=subprocess.PIPE).communicate()[0].decode('utf-8', 'ignore').replace("\n","<br>")
+    if sys.platform.startswith("freebsd") or sys.platform.startswith("netbsd"):
+        options = ["-a", "-q1", "-w1", "-m15"]
+    elif sys.platform.startswith("openbsd"):
+        options = ["-A", "-q1", "-w1", "-m15"]
+    else:  # For Linux
+        options = ["-A", "-q1", "-N32", "-w1", "-m15"]
+    command = traceroute + src + options + [query]
+    result = (
+        subprocess.Popen(command, stdout=subprocess.PIPE)
+        .communicate()[0]
+        .decode("utf-8", "ignore")
+        .replace("\n", "<br>")
+    )
     return result
 
 
@@ -109,7 +140,7 @@ def bird():
     # Just use Bird2
     b = BirdSocket(file=app.config.get("BIRD_SOCKET"))
 
-    query = request.args.get("q","")
+    query = request.args.get("q", "")
     query = unquote(query)
 
     status, result = b.cmd(query)
