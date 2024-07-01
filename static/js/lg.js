674e02bf89b3f06eea3a39c4f51059e66fac4147
Andreas Düring <github@duering-andreas.de>
2024-07-01 19:49:29 +0200

    lg.js: Replace making stuff active

--- static/js/lg.js
+++ static/js/lg.js
@@ -4,6 +4,13 @@ function lg_query(selector) {
 	return document.querySelectorAll(selector);
 }
 
+/**
+ * @returns {Node} This is probably a lie, but I want IntelliSense in VScode
+ */
+function lg_query_single(selector) {
+	return document.querySelector(selector);
+}
+
 function lg_query_do(selector, fun) {
 	lg_query(selector).forEach(ele => { fun(ele); })
 }
@@ -49,9 +56,15 @@ function update_view() {
 
 	lg_query_do(".navbar li", ele => { ele.classList.remove("active"); })
 
-	$(".proto a#" + proto).parent().addClass('active');
-	$(".hosts a[id='" + hosts + "']").parent().addClass('active');
-	$(".request_type a#" + request_type).parent().addClass('active');
+	const node_proto = lg_query_single(".proto a#" + proto);
+	if (node_proto)
+		node_proto.parentElement.classList.add('active');
+	const node_hosts = lg_query_single(".hosts a[id='" + hosts + "']")
+	if (node_hosts)
+		node_hosts.parentElement.classList.add('active')
+	const node_request = lg_query_single(".request_type a#" + request_type);
+	if (node_request)
+		node_request.parentElement.classList.add('active')
 
 	command = $(".request_type a#" + request_type).text().split("...");
 	$(".request_type a:first").html(command[0] + '<b class="caret"></b>');
