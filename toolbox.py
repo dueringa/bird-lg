6a9628eb9e302f61a41fcadbe16758c991fc3e04
Andreas Düring <github@duering-andreas.de>
2024-07-02 19:46:57 +0200

    toolbox: docstrings

--- toolbox.py
+++ toolbox.py
@@ -34,11 +34,13 @@ resolv.lifetime = 1
 resolv.cache = dns_cache
 
 
-def resolve(n, q):
-    return str(resolv.query(n, q)[0])
+def resolve(name, query_type):
+    """Resolve a given name via DNS."""
+    return str(resolv.query(name, query_type)[0])
 
 
 def mask_is_valid(n):
+    """Check whther network mask is valid."""
     if not n:
         return True
     try:
@@ -48,29 +50,33 @@ def mask_is_valid(n):
         return False
 
 
-def ipv4_is_valid(n):
+def ipv4_is_valid(ipv4_adr):
+    """Check whether a given IPv4 address is valid."""
     try:
-        socket.inet_pton(socket.AF_INET, n)
+        socket.inet_pton(socket.AF_INET, ipv4_adr)
         return True
     except socket.error:
         return False
 
 
-def ipv6_is_valid(n):
+def ipv6_is_valid(ipv6_adr):
+    """Check whether a given IPv6 address is valid."""
     try:
-        socket.inet_pton(socket.AF_INET6, n)
+        socket.inet_pton(socket.AF_INET6, ipv6_adr)
         return True
     except socket.error:
         return False
 
 
 def save_cache_pickle(filename, data):
+    """Helper function: Save/Pickle data."""
     output = open(filename, "wb")
     pickle.dump(data, output)
     output.close()
 
 
 def load_cache_pickle(filename, default=None):
+    """Helper function: Load/Pickle data."""
     try:
         pkl_file = open(filename, "rb")
     except IOError:
